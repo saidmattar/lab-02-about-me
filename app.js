87f36df7f622581086688b162b59c55f0e949df6
saidmattar <ssabdou@gmail.com>
2017-04-05 18:29:38 -0700

    added an alert to the user to confirm them how many correct guesses that they got.

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 'use strict';
+var numCorrect = 0;
 var name = prompt('IS your name Adam? Yes/No.');
 console.log('IS your name Adam? Yes/No.');
 if(name.trim().toLowerCase() == 'yes') {
   alert('Hello Adam.');
+  numCorrect++;
 } else {
   alert('OOh sorry about that!');
 }
@@ -10,6 +12,7 @@ var food = prompt('Do you think i like sushi? Yes/No');
 console.log('Do you think i like sushi? Yes/No');
 if ( food.trim().toLowerCase()  == 'yes') {
   alert('You were correct');
+  numCorrect++;
 } else {
   console.log('You missed it this time!');
   alert('You missed it this time!');
@@ -18,6 +21,7 @@ var age = prompt('Are you older than 30 years old? yes/no');
 console.log('Are you older than 30 years old? yes/no');
 if(age.trim().toLowerCase() == 'yes'){
   alert('Welcome to the party');
+  numCorrect++;
 } else {
   console.log('MMMM TOOO YOUNG !');
   alert('MMMM TOOO YOUNG !');
@@ -26,6 +30,7 @@ var coding = prompt('Do you like Codiny? Yes/No');
 console.log('Do you like coding? Yes/No');
 if(coding.trim().toLowerCase() == 'yes'){
   alert('Good for you!');
+  numCorrect++;
 } else {
   alert('Still Good for you!!');
 }
@@ -33,6 +38,7 @@ var workDone = prompt('Are you almost done with your assignment? Yes/No.');
 console.log('Are you almost done with your assignment? yes/no.');
 if(workDone.trim().toLowerCase() == 'yes'){
   alert('Maaan I am not even close');
+  numCorrect++;
 } else {
   alert('Just like me.');
 var myFaveNum = 20;
@@ -42,6 +48,7 @@ for (var i = 0; i < 4; i++) {
     console.log('Great First time go! ');
     alert('Great First time go! ');
     i = 4;
+    numCorrect++;
   } else if (guessMyFaveNum != myFaveNum) {
     console.log('Okay, you have: ' + (3-i) + ' triies left.');
     alert('Okay, you have: ' + (3-i) + ' triies left.');
@@ -49,12 +56,13 @@ for (var i = 0; i < 4; i++) {
     console.log('You got it.');
     alert('You got it.');
     i = 4;
+    numCorrect++;
   }
 }
 }
 var statesIlivedIn = ['New York','Florida', 'California', 'Texas'];
 var trueGuess = false;
-for (var numOfTries = 0; numOfTries < 3; numOfTries++) {
+for (var numOfTries = 0; numOfTries < 6; numOfTries++) {
   console.log('outer for loop', numOfTries);
   var userGuess = prompt(' Can you guess a one State i lived in beside Whashington?!');
 
@@ -65,6 +73,7 @@ for (var numOfTries = 0; numOfTries < 3; numOfTries++) {
      i = statesIlivedIn.length;
      trueGuess = true;
      numOfTries = 3;
+     numCorrect++;
   }
 }
   if(trueGuess == false) {
@@ -72,3 +81,4 @@ for (var numOfTries = 0; numOfTries < 3; numOfTries++) {
    alert('Wrong Guess');
  }
 }
+alert ('great jobe you have Guessed: ' + numCorrect + 'correct answers.');
